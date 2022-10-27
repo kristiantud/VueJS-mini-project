203cc7b7f95351096f851aaa506ddb53f44535c1
Kristian Tud <kristiantud@hotmail.com>
2022-10-27 08:15:44 -0600

    done

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
+// NOTES:
+// we can use any variable created in data() in our main index.html file
 
 
 const app = Vue.createApp({
-    template: '<h1>Hello world! </h1>'
-
+    
+    data(){
+        return {
+            firstName: 'Neo',
+            lastName: '',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+            gender: 'male',
+            email: 'neo@matrix.com'
+        }
+    },
+    methods: {
+        async getUser(){
+            const res = await fetch('https://randomuser.me/api')
+            const {results} = await res.json()
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.picture = results[0].picture.large
+            this.gender = results[0].gender
+            this.email = results[0].email
+        }
+    }
 })
 
 app.mount('#app');
